4598ea81851bb763c3e21438343bca3568ed413b
Elia Menoni <eliamenoni@emenoni.eu>
2025-03-12 21:08:08 +0000

    RINOMINAZIONE APP A GIIC

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GIC.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GIIC.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
